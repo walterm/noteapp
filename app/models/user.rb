bf9a9736022c5afd3f7d0bb102f977ce8b592736
Walter Menendez <walterm@mit.edu>
2013-10-01 21:33:06 -0400

    Updated users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
   has_many :notes
+  
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @email = attributes[:email]
+  end
 end
