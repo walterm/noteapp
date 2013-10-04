0ce22c08a4e06e5b14966f9d44f13a39baec97f1
Walter Menendez <walterm@mit.edu>
2013-10-04 19:59:39 -0400

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-NoteApp::Application.config.secret_token = '025acdfc8c71d4e6d63b8a386afaaa5db2a871d6a513964cd82786762e1bcdf0e0832b5cd14fd628d81b810d92d868c3be68c15bfab1e545a0eddc484b926479'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NoteApp::Application.config.secret_key_base = '081631468fa49133ce35b16d936b6d036da4643219bd17b646ae45ab07ad537ea304de299f7b26b990f7cc3400a336ffad8e90e0411694c3d79edb4fbf60c278'
