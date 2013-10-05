e3886445a180c7fe86c79417877ef9dc675c25ac
Walter Menendez <walterm@mit.edu>
2013-10-04 20:23:33 -0400

    Finished layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 NoteApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/help"
+  get "users/new"
+  root 'pages#home'
+  match '/help/', to: 'pages#help', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
